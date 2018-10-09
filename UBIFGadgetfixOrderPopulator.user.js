25bb90e3c0d1a1cba458e8a5efbcf95ea024f6d5
Ashcall3000 <Ashcall3000@gmail.com>
2018-10-09 00:42:17 -0700

    Create UBIFGadgetfixOrderPopulator.user.js

---
+++ UBIFGadgetfixOrderPopulator.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Christopher Sullivan
+// @include      https://portal.ubif.net/pos/purchasing/edit/*
+// @include      https://gadgetfix.com/customer/order/detail/*
+// @downloadURL  https://github.com/Ashcall3000/UBREAKIFIX/raw/master/UBIFGadgetfixOrderPopulator.user.js
+// @updateURL    https://github.com/Ashcall3000/UBREAKIFIX/raw/master/UBIFGadgetfixOrderPopulator.user.js
+// @run-at document-idle
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
