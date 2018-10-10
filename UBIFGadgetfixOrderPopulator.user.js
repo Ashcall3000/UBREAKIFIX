245d74f16f0c5ac10ccb3fbf7fbdbd5d1ed61aba
Ashcall3000 <Ashcall3000@gmail.com>
2018-10-09 19:42:29 -0700

    Update UBIFGadgetfixOrderPopulator.user.js

--- UBIFGadgetfixOrderPopulator.user.js
+++ UBIFGadgetfixOrderPopulator.user.js
@@ -17,24 +17,32 @@ $(function() {
     'use strict';
 
     // Your code here...
+    var gadget_button_created = false; // Wether button has been added to gadgetfix page.
     var part_list = [];
     var run = setInterval(function() {
         // Runs on the GadgetFix page to put all part items into an array
         if ($("table.table-border")[0] != null) {
             // Add button to page
-            $("#lhc_status_container").innerHTML += '<button id="copy" style="position: fixed; '
-                + 'z-index: 5; right: 1px; top: 100px; background-color: #4CAF50; border: none;'
-                + 'color: white;">COPY</button>';
-            $("table.table-border > tbody > tr").each(function(el) {
-                var item_num = $(el).find("p").first().innerHTML;
-                var price_amoun = $(el).find("i-right").first().innerHTML;
-                var quan_amoun = $(el).find("i-center").first().innerHTML;
-                var total_amoun = $(el).find("i-right").eq(1).innerHTML;
-                part_list.push(new part(item_num, price_amoun, quan_amoun, total_amoun);
+            if (!gadget_button_created) {
+                $("#lhc_status_container").innerHTML += '<button id="copy" style="position: fixed; '
+                    + 'z-index: 5; right: 1px; top: 100px; background-color: #4CAF50; border: none;'
+                    + 'color: white;">COPY</button>';
+                gadget_button_created = true;
+                $("#copy").bind('click', getTableGadget());
             }
         }
     }, 10000); // Runs every ten seconds
 })();
+            
+function getTableGadget() {
+    $("table.table-border > tbody > tr").each(function(el) {
+        var item_num = $(el).find("p").first().innerHTML;
+        var price_amoun = $(el).find("i-right").first().innerHTML;
+        var quan_amoun = $(el).find("i-center").first().innerHTML;
+        var total_amoun = $(el).find("i-right").eq(1).innerHTML;
+        part_list.push(new part(item_num, price_amoun, quan_amoun, total_amoun);
+    }
+}
 
 function part(item, price, quan, total) {
     this.item = item; // Item number from GadgetFix
