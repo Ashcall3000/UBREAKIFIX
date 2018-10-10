3532c75e063d4644afc3bda50d692022060c85c5
Ashcall3000 <Ashcall3000@gmail.com>
2018-10-09 23:31:34 -0700

    Update UBIFPortalAutoFill.user.js

--- UBIFPortalAutoFill.user.js
+++ UBIFPortalAutoFill.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         UBIF Portal Auto Fill Script
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Auto fills update notes to expidite the procedure.
 // @author       Christopher Sullivan
-// @include      https://portal.ubif.net/pos/checkout-new/*
+// @include      https://portal.ubif.net/*
 // @downloadURL  https://github.com/Ashcall3000/UBREAKIFIX/raw/master/UBIFPortalAutoFill.user.js
 // @updateURL    https://github.com/Ashcall3000/UBREAKIFIX/raw/master/UBIFPortalAutoFill.user.js
 // @run-at document-idle
@@ -13,49 +13,52 @@
 
 (function() {
     'use strict';
-    
-    var status = document.getElementsByClassName("editor-add-in")[0].value;
-    var text = "";
-    var event = new Event('change', { bubbles: true }); // Event used to update changes for page.
-    var run = setInterval(function() {
-        var new_status = document.getElementsByClassName("editor-add-in")[0].value;
-        if (new_status != status) {
-            switch (new_status) {
-                case 0:
-                    text = '"Device has been repaired and passed tests."';
-                    break;
-                case 1:
-                    text = '"Awaiting callback from the customer."';
-                    break;
-                case 2:
-                    text = '"Awaiting for customer to bring in the device."';
-                    break;
-                case 6:
-                    text = '"Customer has declined the repair and has upto 30 days to pickup there device before it is recycled."';
-                    break;
-                case 7:
-                    text = '"Customer has abandoned the device and is sloted to be recycled."';
-                    break;
-                case 9:
-                    text = '"Need to order a part for the device. Will take 3 to 5 buisness days for shipping."';
-                    break;
-                case 10:
-                    text = '"Device is currently being repaired."';
-                    break;
-                case 11:
-                    text = '"Device is repaired and ready for pickup."';
-                    break;
-                case 12:
-                    text = '"Device was not able to be repaired and is ready for pickup."';
-            }
-            status = new_status;
-            document.getElementsByClassName("note-placeholder")[0].style = "display: none;";
-            document.getElementsByClassName("btn-confirm")[4].disabled = false;
-            var text_area = document.getElementsByClassName("note-editable")[0];
-            text_area.innerHTML = text;
-            text_area.dispatchEvent(event);
+    var start = setInterval(function() {
+        if (window.location.href.includes("https://portal.ubif.net/pos/checkout-new/") {
+            var status = document.getElementsByClassName("editor-add-in")[0].value;
+            var text = "";
+            var event = new Event('change', { bubbles: true }); // Event used to update changes for page.
+            var run = setInterval(function() {
+                var new_status = document.getElementsByClassName("editor-add-in")[0].value;
+                if (new_status != status) {
+                    switch (new_status) {
+                        case 0:
+                            text = '"Device has been repaired and passed tests."';
+                            break;
+                        case 1:
+                            text = '"Awaiting callback from the customer."';
+                            break;
+                        case 2:
+                            text = '"Awaiting for customer to bring in the device."';
+                            break;
+                        case 6:
+                            text = '"Customer has declined the repair and has upto 30 days to pickup there device before it is recycled."';
+                            break;
+                        case 7:
+                            text = '"Customer has abandoned the device and is sloted to be recycled."';
+                            break;
+                        case 9:
+                            text = '"Need to order a part for the device. Will take 3 to 5 buisness days for shipping."';
+                            break;
+                        case 10:
+                            text = '"Device is currently being repaired."';
+                            break;
+                        case 11:
+                            text = '"Device is repaired and ready for pickup."';
+                            break;
+                        case 12:
+                            text = '"Device was not able to be repaired and is ready for pickup."';
+                    }
+                    status = new_status;
+                    document.getElementsByClassName("note-placeholder")[0].style = "display: none;";
+                    document.getElementsByClassName("btn-confirm")[4].disabled = false;
+                    var text_area = document.getElementsByClassName("note-editable")[0];
+                    text_area.innerHTML = text;
+                    text_area.dispatchEvent(event);
+                }
+            }, 1000); // Checks every second.
         }
-    }, 1000); // Checks every second.
+    }, 10000); // Checks every 10 seconds.
     /*
     0 Awaiting Approval
     1 Aw Call
