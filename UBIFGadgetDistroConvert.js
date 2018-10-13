a60318415e020e70dd98acb037ea9f5ad5ec2dce
Ashcall3000 <Ashcall3000@gmail.com>
2018-10-13 11:17:56 -0700

    Update UBIFGadgetDistroConvert.js

--- UBIFGadgetDistroConvert.js
+++ UBIFGadgetDistroConvert.js
@@ -2,6 +2,14 @@
  * Script that converts gadgetfix item numbers to ubreakifix distro item numbers
  */
  
- function convert(item) {
- 
- }
+function convert(item) {
+    var hash = {};
+    hash['351309799887'] = "8218"; // iPhone 5 Back Camera
+    hash['371254701166'] = "8225"; // iPhone 5 Dock Connector Black
+    hash['350778276217'] = "8226"; // iPhone 5 Dock Connector White
+    hash['231475279944'] = "8217"; // iPhone 5 Prox
+    hash['351309802008'] = "8229"; // iPhone 5 Volume Button Flex
+    hash[''] = "";
+    hash[''] = "";
+    hash[''] = "";
+}
