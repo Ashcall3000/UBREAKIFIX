77a56b50f6fdbb6b5c7471db94a108c25be14568
Ashcall3000 <Ashcall3000@gmail.com>
2018-10-15 20:46:01 -0700

    Update UBIFPortalAutoFill.user.js

--- UBIFPortalAutoFill.user.js
+++ UBIFPortalAutoFill.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UBIF Portal Auto Fill Script
 // @namespace    http://tampermonkey.net/
-// @version      1.1.5
+// @version      1.1.6
 // @description  Auto fills update notes to expidite the procedure.
 // @author       Christopher Sullivan
 // @include      https://portal.ubif.net/*
@@ -16,52 +16,55 @@
 
     var start = setInterval(function() {
         if (window.location.href.includes("https://portal.ubif.net/pos/checkout-new/")) {
-            var status = document.getElementsByClassName("editor-add-in")[0].value;
-            var run = setInterval(function() {
-                var new_status = document.getElementsByClassName("editor-add-in")[0].value;
-                if (new_status != status) {
-                    var val_list = document.querySelectorAll("select.editor-add-in option");
-                    var val = "";
-                    val_list.forEach(function(el) {
-                        if (el.value == new_status) {
-                            val = el.innerText;
+            var check_status = document.getElementsByClassName("editor-add-in")[0];
+            if (check_status != null) {
+                var status = document.getElementsByClassName("editor-add-in")[0].value;
+                var run = setInterval(function() {
+                    var new_status = document.getElementsByClassName("editor-add-in")[0].value;
+                    if (new_status != status) {
+                        var val_list = document.querySelectorAll("select.editor-add-in option");
+                        var val = "";
+                        val_list.forEach(function(el) {
+                            if (el.value == new_status) {
+                                val = el.innerText;
+                            }
+                        });
+                        switch (val) {
+                            case "Awaiting Approval":
+                                setText("none", false, "Device has been repaired and passed tests.");
+                                break;
+                            case "Awaiting Callback":
+                                setText("none", false, "Awaiting callback from the customer.");
+                                break;
+                            case "Awaiting Device":
+                                setText("none", false, "Awaiting for the customer to bring in their device.");
+                                break;
+                            case "Declined - RFP":
+                                setText("none", false, "Customer has declined the repair and has upto 30 days to pickup there device before it is recycled.");
+                                break;
+                            case "Device Abandoned":
+                                setText("none", false, "Customer has abandoned the device and is sloted to be recycled.");
+                                break;
+                            case "Need to Order Parts":
+                                setText("none", false, "Need to order parts for the device. Will take 3 to 5 business days for shipping.");
+                                break;
+                            case "Diag in Progress":
+                                setText("none", false, "Currently diagnosing the device to give customer a repair quote.");
+                                break;
+                            case "Repair in Progress":
+                                setText("none", false, "The device is currently being repaired.");
+                                break;
+                            case "Repaired - RFP":
+                                setText("none", false, "The device is repaired and ready for pickup.");
+                                break;
+                            case "Unrepairable - RFP":
+                                setText("none", false, "We were not able to repair the device and is ready for pickup. If not picked up within 30 days will be slotted to be recycled.");
+                                break;
+                            default:
+                                setText("block", true, "");
                         }
-                    });
-                    switch (val) {
-                        case "Awaiting Approval":
-                            setText("none", false, "Device has been repaired and passed tests.");
-                            break;
-                        case "Awaiting Callback":
-                            setText("none", false, "Awaiting callback from the customer.");
-                            break;
-                        case "Awaiting Device":
-                            setText("none", false, "Awaiting for the customer to bring in their device.");
-                            break;
-                        case "Declined - RFP":
-                            setText("none", false, "Customer has declined the repair and has upto 30 days to pickup there device before it is recycled.");
-                            break;
-                        case "Device Abandoned":
-                            setText("none", false, "Customer has abandoned the device and is sloted to be recycled.");
-                            break;
-                        case "Need to Order Parts":
-                            setText("none", false, "Need to order parts for the device. Will take 3 to 5 business days for shipping.");
-                            break;
-                        case "Diag in Progress":
-                            setText("none", false, "Currently diagnosing the device to give customer a repair quote.");
-                            break;
-                        case "Repair in Progress":
-                            setText("none", false, "The device is currently being repaired.");
-                            break;
-                        case "Repaired - RFP":
-                            setText("none", false, "The device is repaired and ready for pickup.");
-                            break;
-                        case "Unrepairable - RFP":
-                            setText("none", false, "We were not able to repair the device and is ready for pickup. If not picked up within 30 days will be slotted to be recycled.");
-                            break;
-                        default:
-                            setText("block", true, "");
+                        status = new_status;
                     }
-                    status = new_status;
                 }
             }, 1000); // Checks every second.
         }
