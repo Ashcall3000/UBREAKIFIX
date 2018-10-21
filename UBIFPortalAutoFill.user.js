19508b7b9730c67489b3dad01bed9e48cbd4d02e
Ashcall3000 <Ashcall3000@gmail.com>
2018-10-21 12:33:52 -0700

    Update UBIFPortalAutoFill.user.js

--- UBIFPortalAutoFill.user.js
+++ UBIFPortalAutoFill.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UBIF Portal Auto Fill Script
 // @namespace    http://tampermonkey.net/
-// @version      1.1.6
+// @version      1.1.7
 // @description  Auto fills update notes to expidite the procedure.
 // @author       Christopher Sullivan
 // @include      https://portal.ubif.net/*
@@ -30,37 +30,37 @@
                     });
                     switch (val) {
                         case "Awaiting Approval":
-                            setText("none", false, "Device has been repaired and passed tests.");
+                            setText("none", "Device has been repaired and passed tests.");
                             break;
                         case "Awaiting Callback":
-                            setText("none", false, "Awaiting callback from the customer.");
+                            setText("none", "Awaiting callback from the customer.");
                             break;
                         case "Awaiting Device":
-                            setText("none", false, "Awaiting for the customer to bring in their device.");
+                            setText("none", "Awaiting for the customer to bring in their device.");
                             break;
                         case "Declined - RFP":
-                            setText("none", false, "Customer has declined the repair and has upto 30 days to pickup there device before it is recycled.");
+                            setText("none", "Customer has declined the repair and has upto 30 days to pickup there device before it is recycled.");
                             break;
                         case "Device Abandoned":
-                            setText("none", false, "Customer has abandoned the device and is sloted to be recycled.");
+                            setText("none", "Customer has abandoned the device and is sloted to be recycled.");
                             break;
                         case "Need to Order Parts":
-                            setText("none", false, "Need to order parts for the device. Will take 3 to 5 business days for shipping.");
+                            setText("none", "Need to order parts for the device. Will take 3 to 5 business days for shipping.");
                             break;
                         case "Diag in Progress":
-                            setText("none", false, "Currently diagnosing the device to give customer a repair quote.");
+                            setText("none", "Currently diagnosing the device to give customer a repair quote.");
                             break;
                         case "Repair in Progress":
-                            setText("none", false, "The device is currently being repaired.");
+                            setText("none", "The device is currently being repaired.");
                             break;
                         case "Repaired - RFP":
-                            setText("none", false, "The device is repaired and ready for pickup.");
+                            setText("none", "The device is repaired and ready for pickup.");
                             break;
                         case "Unrepairable - RFP":
-                            setText("none", false, "We were not able to repair the device and is ready for pickup. If not picked up within 30 days will be slotted to be recycled.");
+                            setText("none", "We were not able to repair the device and is ready for pickup. If not picked up within 30 days will be slotted to be recycled.");
                             break;
                         default:
-                            setText("block", true, "");
+                            setText("block", "");
                     }
                     status = new_status;
                 }
@@ -81,9 +81,8 @@ function eventFire(el, etype) {
     }
 }
 
-function setText(disp, disa, text) {
+function setText(disp, text) {
     document.getElementsByClassName("note-placeholder")[0].style = "display: " + disp + ";";
-    document.getElementsByClassName("btn-confirm")[4].disabled = disa;
     var text_area = document.getElementsByClassName("note-editable")[0];
     text_area.innerHTML = text;
     eventFire(text_area, 'input');
