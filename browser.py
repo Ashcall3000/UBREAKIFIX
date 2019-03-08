331b24f1c9cba12f46fb0afe3fc6c8acf54e47b5
Ashcall3000 <Ashcall3000@gmail.com>
2019-03-08 00:22:48 -0800

    Update browser.py

--- browser.py
+++ browser.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotInteractableException
 from selenium.common.exceptions import ElementClickInterceptedException
 
 
@@ -20,11 +21,11 @@ class FireBrowser(object):
         fp.set_preference("browser.tabs.remote.autostart.1", False)
         fp.set_preference("browser.tabs.remote.autostart.2", False)
         self.browser = webdriver.Firefox(firefox_profile=fp)
-        self.tab_names = [""]
+        self.tab_names = []
         self.current_tab = 0
         self.open = True
 
-    def load(self, url, tab_name, tab=0):
+    def load(self, url: str, tab_name: str, tab=0):
         """Loads a new page in the given tab. If the tab number is -1
         then it will load the page in a new tab. Reopens Browser if it
         is closed.
@@ -38,7 +39,7 @@ class FireBrowser(object):
         if tab is -1:  # Loads page to a new tab
             self.new_tab(url, tab_name)
         elif tab is not self.current_tab:
-            self.set_tab(tab)  # Will set the current tab variable
+            self.switch_tab(tab)  # Will set the current tab variable
             self.load(url, tab_name, self.current_tab)
         else:
             self.urls[tab] = url
@@ -71,7 +72,7 @@ class FireBrowser(object):
                 self.current_tab = len(self.urls) - 1
         return self.current_tab
 
-    def find_tab(self, tab_name):
+    def find_tab(self, tab_name: str):
         """Finds the tab with the tab name given and returns the tab number.
         If tab can't be found then returns -1.
 
@@ -83,7 +84,7 @@ class FireBrowser(object):
                 return i
         return -1
 
-    def set_tab(self, tab):
+    def switch_tab(self, tab):
         """Sets the current tab to a new tab using either tab name or
         tab number.
 
@@ -97,29 +98,39 @@ class FireBrowser(object):
                 self.current_tab = tab
                 self.browser.switch_to.window(self.browser.window_handles[tab])
 
-    def get_element(self, selector) -> WebElement:
+    def get_element(self, selector, element=False) -> WebElement:
         """Finds element in the browser using a CSS selector then returns that
-        WebElement. If element is not found on the page will return false.
+        WebElement. If given a WebElement will then css search in that element.
+         If element is not found on the page will return false.
 
         Args:
-            selector: CSS String"""
+            selector: CSS String
+            element: WebElement DEFAULT: False"""
         try:
-            return self.browser.find_element_by_css_selector(selector)
+            if element:
+                return element.find_element_by_css_selector(selector)
+            else:
+                return self.browser.find_element_by_css_selector(selector)
         except NoSuchElementException:
             pyprint.warning(selector, warn="ELEMENT NOT FOUND")
             return False
 
-    def get_elements(self, selector) -> list:
+    def get_elements(self, selector, element=False) -> list:
         """Finds elements in the browser using a css selector then returns those
-        WebElements. If elements are not found on the page will return false.
+        WebElements. If given a WebElement will then css search in that element.
+        If elements are not found on the page will return empty list.
 
         Args:
-            selector: CSS String"""
+            selector: CSS String
+            element: WebElement DEFAULT: False"""
         try:
-            return self.browser.find_elements_by_css_selector(selector)
+            if element:
+                return element.find_element_by_css_selector(selector)
+            else:
+                return self.browser.find_elements_by_css_selector(selector)
         except NoSuchElementException:
             pyprint.warning(selector, warn="ELEMENTS NOT FOUND")
-            return False
+            return []
 
     def check_selector(self, selector) -> bool:
         """Given a CSS Selector tries to find element in page.
@@ -143,38 +154,64 @@ class FireBrowser(object):
             self.browser.quit()
             self.__print()
 
-    def click(self, selector):
+    def click(self, selector, sleep_time=2) -> bool:
         """Clicks the given object. Selector can either be CSS string or
-        it can be an element from selenium.
+        it can be an element from selenium. At the end after the click the
+        program will pause the amount of sleep_time. Returns boolean if
+        program was able to click element or not.
 
         Args:
-            selector: Either CSS Selector or Element Object."""
+            selector: Either CSS Selector or Element Object.
+            sleep_time: Time in seconds. DEFAULT: 2"""
         try:
             if type(selector) is str:
                 selector = self.get_element(selector)
                 if selector:
                     selector.click()
+                    sleep(sleep_time)
+                    return True
             elif type(selector) is bool:
                 pyprint.warning(selector, warn='CLICK FAILURE')
+                return False
             else:
                 selector.click()
+                sleep(sleep_time)
+                return True
         except ElementClickInterceptedException:
-            pyprint.warning(selector, warn='CLICK FAILURE')
-        sleep(2)
+            pyprint.warning(selector, warn='CLICK INTERCEPTED FAILURE')
+            return False
+        except ElementNotInteractableException:
+            pyprint.warning(selector, warn='CLICK INTERACTABLE FAILURE')
 
-    def send_text(self, text, selector):
+    def send_text(self, text: str, selector) -> bool:
         """Sends text to a specific element. Selector can either be CSS string or
-        it can be an element from selenium.
+        it can be an element from selenium. Returns bool if was able to write to
+        element or not.
 
         Args:
             text: Text that will be sent to the element.
             selector: Either CSS Selector or Element Object."""
-        if type(selector) is str:
-            if self.check_selector(selector):
-                selector = self.browser.find_element_by_css_selector(selector)
+        try:
+            if type(selector) is str:
+                if self.check_selector(selector):
+                    selector = self.browser.find_element_by_css_selector(selector)
+                    selector.send_keys(text)
+                    return True
+            else:
                 selector.send_keys(text)
-        else:
-            selector.send_keys(text)
+                return True
+        except ElementNotInteractableException:
+            pyprint.warning(selector, warn='SEND TEXT FAILURE')
+            return False
+
+    def remind_me_later(self):
+        """Checks to see if the Remind Me Later button is on the screen and clicks it."""
+        while self.check_selector("button.show"):
+            buttons = self.get_elements("button.show")
+            if len(buttons) > 1:
+                self.click(buttons[1])
+            else:
+                self.click(buttons[0])
 
     def __print(self):
         pyprint.midline("BROWSER DATA", mid=":")
