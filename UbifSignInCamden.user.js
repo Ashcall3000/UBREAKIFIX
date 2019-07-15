9cb613adcfb0ba58425c9580c4f5a830937a18df
Ashcall3000 <Ashcall3000@gmail.com>
2019-07-14 18:12:04 -0700

    Update UbifSignInCamden.user.js

--- UbifSignInCamden.user.js
+++ UbifSignInCamden.user.js
@@ -1,13 +1,10 @@
 // ==UserScript==
 // @name         UbifSignInCamden
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Creates buttons on the sign in popup to select which user you are.
 // @author       Christopher Sullivan
 // @include        https://portal.ubif.net/*
-// @downloadURL  https://github.com/Ashcall3000/UBREAKIFIX/raw/master/UbifSignInCamden.user.js
-// @updateURL    https://github.com/Ashcall3000/UBREAKIFIX/raw/master/UbifSignInCamden.user.js
-// @grant        none
 // ==/UserScript==
 
 
@@ -16,34 +13,36 @@ var passcodes = ['519880', '791605', '164438', '200211'];
 
 (function() {
     'use strict';
-
+    
     var test = true; // Program hasn't run yet.
     // Run program every second.
     var run = setInterval(function() {
-        if (checkElement(".modal-sm") && test) {
+        if (document.getElementsByClassName("modal-sm")[0] != null && test) {
             test = false;
-            var element = findElement(".modal-sm");
-            if (findElementByText(".modal-title", "Enter your PIN")) {
-                var location = findElement("div.modal-content:nth-child(1)");
+            var element = document.getElementsByClassName("modal-sm")[0];
+            if (element.querySelector(".modal-title").innerText == "Enter your PIN") {
+                var location = element.querySelector("div.modal-content:nth-child(1)");
                 var para = document.createElement("div");
                 para.className = "modal-body";
                 var text = "";
                 for (var i = 0; i < names.length; i++) {
-                    text += '<button id="' + i + '" class="btn btn-cancel push-left fastclickable">' + names[i] + '</button>';
+                     text += "<button id=\"" + i + "\">" + names[i] + "</button>";
                 }
                 location.appendChild(para);
                 para.innerHTML += text;
-                for (var i = 0; i < name.length; i++) {
-                    findElement('#' + i).addEventListener("click", function() {
-                        var element = findElement("#pin-entry");
-                        setField(element, "input", passcodes[this.id]);
-                        eventFire("button.btn:nth-child(2)", "click");
-                    });
+                for (var j = 0; j < names.length; j++) {
+                    document.getElementById(j).addEventListener("click", buttonClick);
                 }
-            } else if (!test && checkElement(".modal-sm")) {
-                test = false;
             }
+        } else if (!test && document.getElementsByClassName("modal-sm")[0] == null) {
+            test = true;
         }
     }, 1000)
 })();
-
+function buttonClick() {
+    console.log(passcodes[this.id]);
+    var element = document.getElementById("pin-entry");
+    element.value = passcodes[this.id];
+    element.dispatchEvent(new Event("input", {bubbles:true}));
+    document.querySelector("button.btn:nth-child(2)").dispatchEvent(new Event("click", {bubbles:true}));
+}
