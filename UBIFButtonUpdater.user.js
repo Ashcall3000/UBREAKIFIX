3616de56993558cc7a7b837fad6296650e90405f
Ashcall3000 <Ashcall3000@gmail.com>
2020-04-16 02:10:26 -0700

    Create UBIFButtonUpdater.user.js

---
+++ UBIFButtonUpdater.user.js
@@ -0,0 +1,140 @@
+// ==UserScript==
+// @name         UBIF Button Updater
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Adds a button to assign the workorder to the current user.
+// @author       Christopher Sullivan
+// @include      https://portal.ubif.net/*
+// @require      https://github.com/Ashcall3000/UBREAKIFIX/raw/master/FindElement.js
+// @downloadURL  https://github.com/Ashcall3000/UBREAKIFIX/raw/master/UBIFButtonUpdater.user.js
+// @updateURL    https://github.com/Ashcall3000/UBREAKIFIX/raw/master/UBIFButtonUpdater.user.js
+// @run-at document-idle
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var created = false;
+    var button_text = "";
+    var note_button = '#paneled-side-bar > div > div.bar-buttons > button.btn.blue.fastclickable';
+    var run = setInterval(function() {
+        if (checkElement(note_button) && !created) {
+            created = true;
+            var el = makeElement(findElement('#wrap > div > div.portal-header.row > div.col-xs-12.col-md-6.portal-actions > div'), 'button', 'auto_note_button', 'btn blue fastclickable', 'Text');
+            el.style.backgroundColor='#269CD8';
+            el.style.color='white';
+            el.addEventListener('click', autoUpdate);
+            setButtonText();
+        } else if (!checkElement(note_button) && created) {
+            created = false;
+        } else {
+            if (checkElement('#auto_note_button')) {
+                var elm = findElement('#auto_note_button');
+                var current_status = findElement('#wrap > div > div.portal-pos.with-sidebar > div > div > workorder-tab-list > div > ul > li.active > a > tab-heading > div > div:nth-child(4) > span').textContent;
+                if (elm.textContent == current_status) {
+                    setButtonText();
+                } else if (elm.textContent == "Not Available") {
+                    setButtonText();
+                } else if (checkSamsungOrGoogle() != 0) {
+                    setButtonText();
+                }
+            }
+        }
+    }, 500);
+})();
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+function getSwitchText(status) {
+    var check = checkSamsungOrGoogle();
+    var gspn = true;
+    if (check == 1) {
+        if (checkElement('#portal-alert-bar > div > div > span > button') && findElement("#portal-alert-bar > div > div > span > button").textContent.includes("GSPN")) {
+            gspn = false;
+        }
+    }
+    if (status == "Awaiting Diag") {
+        return "Diag in Progress";
+    } else if (status == "Awaiting Repair") {
+        if (check == 1 && !gspn) {
+            return "Please Create GSPN";
+        }
+        return "Repair in Progress";
+    } else if (status == "Diag in Progress") {
+        return "Repair in Progress";
+    } else if (status == "Repair in Progress") {
+        if (check == 1 || check == 2) {
+            return "Quality Inspection";
+        } else {
+            return "Repaired - RFP";
+        }
+    } else if (status == "Quality Inspection" && check != 1 && check != 2) {
+        return "Repaired - RFP";
+    } else {
+        return "Not Available";
+    }
+}
+
+function checkSamsungOrGoogle() {
+    var samsung = '#wrap > div > div.portal-pos.with-sidebar > div > div > workorder-tab-list > div > div > div.tab-pane.active > div.row.bg-white.sale-items.with-shadow > div > div.card-title.partner-wo.showing-warnings > div.partner-hold > div';
+    var google = '#portal-alert-bar > div > div.alert.portal-alert.alert-danger > span > button';
+    if (checkElement(samsung) && findElement(samsung).textContent.includes("Samsung")) {
+        console.log("Samsung");
+        return 1;
+    } else if (checkElement(google) && findElement(google).textContent.includes("Diag App")) {
+        return 2;
+    } else {
+        console.log("Not Google or Samsung");
+        return 0;
+    }
+}
+
+function setButtonText() {
+    var el = findElement("#auto_note_button");
+    var current_status = findElement('#wrap > div > div.portal-pos.with-sidebar > div > div > workorder-tab-list > div > ul > li.active > a > tab-heading > div > div:nth-child(4) > span').textContent;
+    el.textContent = getSwitchText(current_status);
+    if (el.textContent == "Not Available" || el.textContent == "Please Create GSPN") {
+        el.disabled = true;
+    } else {
+        el.disabled = false;
+    }
+}
+
+function autoUpdate() {
+    var current_status = findElement('#wrap > div > div.portal-pos.with-sidebar > div > div > workorder-tab-list > div > ul > li.active > a > tab-heading > div > div:nth-child(4) > span').textContent;
+    var note_button = '#paneled-side-bar > div > div.bar-buttons > button.btn.blue.fastclickable';
+    eventFire(note_button, 'click');
+    sleep(250).then(() => {
+        var text = getSwitchText(current_status);
+        var els = findElements("div.extra-actions > select > option");
+        var to_select = 0;
+        for ( ; to_select < els.length; to_select++) {
+            if (els[to_select].textContent == text) {
+                findElement("div.extra-actions > select").value = to_select;
+                break;
+            }
+        }
+        angular.element('div.extra-actions > select').triggerHandler('change');
+    })
+    sleep(2000).then(() => {
+        eventFire('#custom-tabset > div.panel-body > div > div.tab-pane.active > div:nth-child(1) > sales-text-editor-buttons > div.buttons-hold.clearfix > div.right-buttons > button', 'click');
+    })
+    setButtonText();
+}
+
+function makeElement(loc, tag, id='', el_class='', text='') {
+    var elm = document.createElement(tag);
+    if (id != '') {
+        elm.setAttribute('id', id);
+    }
+    if (el_class != '') {
+        elm.setAttribute('class', el_class);
+    }
+    if (text != '') {
+        elm.innerText = text;
+    }
+    loc.prepend(elm);
+    return elm;
+}
