95ede99da1d409313603d9ad76da3e31444bf226
Ashcall3000 <Ashcall3000@gmail.com>
2020-04-27 01:04:26 -0700

    Update UBIFPortalAutoFill.user.js

--- UBIFPortalAutoFill.user.js
+++ UBIFPortalAutoFill.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UBIF Portal Auto Fill Script
 // @namespace    http://tampermonkey.net/
-// @version      1.2.4
+// @version      1.3.0
 // @description  Auto fills update notes to expidite the procedure.
 // @author       Christopher Sullivan
 // @include      https://portal.ubif.net/*
@@ -15,10 +15,41 @@
 (function() {
     'use strict';
 
+    var created = false;
+    var button_text = "";
+    var note_button = '#paneled-side-bar > div > div.bar-buttons > button.btn.blue.fastclickable';
+    var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
     var ran = false;
     var status = null;
     var user_set = false;
     var start = setInterval(function() {
+        // Auto Button
+        if (isChrome) {
+            if (checkElement(note_button) && !created) {
+                created = true;
+                var el = makeElement(findElement('#wrap > div > div.portal-header.row > div.col-xs-12.col-md-6.portal-actions > div'), 'button', 'auto_note_button', 'btn blue fastclickable', 'Text');
+                el.style.backgroundColor='#269CD8';
+                el.style.color='white';
+                el.addEventListener('click', autoUpdate);
+                setButtonText();
+            } else if (!checkElement(note_button) && created) {
+                created = false;
+            } else {
+                if (checkElement('#auto_note_button')) {
+                    var elm = findElement('#auto_note_button');
+                    var current_status = findElement('#wrap > div > div.portal-pos.with-sidebar > div > div > workorder-tab-list > div > ul > li.active > a > tab-heading > div > div:nth-child(4) > span').textContent;
+                    if (elm.textContent == current_status) {
+                        setButtonText();
+                    } else if (elm.textContent == "Not Available") {
+                        setButtonText();
+                    } else if (checkSamsungOrGoogle() != 0) {
+                        setButtonText();
+                    }
+                }
+            }
+        }
+        // Auto Fill
         if (checkURL("https://portal.ubif.net/pos/checkout-new/") && checkElement(".editor-add-in")) {
             if (!ran) {
                 status = findElement(".editor-add-in").value;
@@ -134,4 +165,130 @@ function setText(disp, text) {
     findElement(".note-placeholder").style = "display: " + disp + ";";
     findElement(".note-editable").innerHTML = text;
     setField(".note-editable", 'input', text);
+}
+
+function getSwitchText(status) {
+    var check = checkSamsungOrGoogle();
+    var gspn = true;
+    if (check == 1) {
+        if (findElementByText('button', 'GSPN')) {
+            gspn = false;
+        }
+    }
+    if (status == "Awaiting Diag") {
+        return "Diag in Progress";
+    } else if (status == "Awaiting Repair") {
+        if (check == 1 && !gspn) {
+            return "Auto Create GSPN";
+        }
+        return "Repair in Progress";
+    } else if (status == "Diag in Progress") {
+        return "Repair in Progress";
+    } else if (status == "Repair in Progress") {
+        if (check == 1 || check == 2 || check == 3) {
+            return "Quality Inspection";
+        } else {
+            return "Repaired - RFP";
+        }
+    } else if (status == "Quality Inspection" && check != 1 && check != 2) {
+        return "Repaired - RFP";
+    } else {
+        return "Not Available";
+    }
+}
+
+function checkSamsungOrGoogle() {
+    var google = '#portal-alert-bar > div > div.alert.portal-alert.alert-danger > span > button';
+    if (findElementByText('div.partner-hold', 'SAMSUNG')) {
+        return 1;
+    } else if (checkElement(google) && findElement(google).textContent.includes("Diag App")) {
+        return 2;
+    } else if (findElementByText('div.partner-hold', 'ASURION')) {
+        return 3;
+    } else {
+        console.log("Not Google or Samsung");
+        return 0;
+    }
+}
+
+function setButtonText() {
+    var el = findElement("#auto_note_button");
+    var current_status = findElement('#wrap > div > div.portal-pos.with-sidebar > div > div > workorder-tab-list > div > ul > li.active > a > tab-heading > div > div:nth-child(4) > span').textContent;
+    el.textContent = getSwitchText(current_status);
+    if (el.textContent == "Not Available") {
+        el.disabled = true;
+    } else {
+        el.disabled = false;
+    }
+}
+
+function autoUpdate() {
+    var current_status = findElement('#wrap > div > div.portal-pos.with-sidebar > div > div > workorder-tab-list > div > ul > li.active > a > tab-heading > div > div:nth-child(4) > span').textContent;
+    var note_button = '#paneled-side-bar > div > div.bar-buttons > button.btn.blue.fastclickable';
+    var gspn_div = 'body > div.modal.fade.fastclickable.portal-base.repair-ticket-create.in > div > div > div.modal-body';
+    var running = false;
+    if (findElement('#auto_note_button').textContent.includes('GSPN')) {
+        eventFire('button', 'click', 'GSPN');
+        console.log('Clicked GSPN');
+        var create_repair_ticket = setInterval(function() {
+            if (findElementByText('.modal-dialog button', 'Create Repair Ticket')) {
+                eventFire('.modal-dialog button', 'click', 'Create Repair Ticket');
+                clearInterval(create_repair_ticket);
+            }
+        }, 250);
+        var close_ticket_window = setInterval(function() {
+            if (findElementByText('.modal-dialog button', 'Close')) {
+                eventFire('.modal-dialog button', 'click', 'Close');
+                clearInterval(close_ticket_window);
+            }
+        }, 250);
+        var note_button_interval = setInterval(function() {
+            if (checkElement(note_button) && !checkElement(gspn_div)) {
+                eventFire(note_button, 'click');
+                clearInterval(note_button_interval);
+            }
+        }, 250);
+    } else {
+        eventFire(note_button, 'click');
+    }
+    var status_change_interval = setInterval(function() {
+        console.log('Status change interval');
+        if (!checkElement(gspn_div) && !checkElement('#paneled-side-bar.closed') && !running) {
+            console.log('IF statement for status change interval');
+            running = true;
+            sleep(250).then(() => {
+                var text = getSwitchText(current_status);
+                var els = findElements("div.extra-actions > select > option");
+                var to_select = 0;
+                for ( ; to_select < els.length; to_select++) {
+                    if (els[to_select].textContent == text) {
+                        findElement("div.extra-actions > select").value = to_select;
+                        break;
+                    }
+                }
+                angular.element('div.extra-actions > select').triggerHandler('change');
+            })
+            sleep(800).then(() => {
+                eventFire('#custom-tabset > div.panel-body > div > div.tab-pane.active > div:nth-child(1) > sales-text-editor-buttons > div.buttons-hold.clearfix > div.right-buttons > button', 'click');
+            })
+            setButtonText();
+            running = false;
+            clearInterval(status_change_interval);
+        }
+    }, 500);
+}
+
+function makeElement(loc, tag, id='', el_class='', text='') {
+    var elm = document.createElement(tag);
+    if (id != '') {
+        elm.setAttribute('id', id);
+    }
+    if (el_class != '') {
+        elm.setAttribute('class', el_class);
+    }
+    if (text != '') {
+        elm.innerText = text;
+    }
+    loc.prepend(elm);
+    return elm;
 }
